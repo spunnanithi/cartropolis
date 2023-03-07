74e341bee5cd8b5ac7f4a21b9d8d76fb13b190cc
Sirasit Punnanithi <spunnanithi@gmail.com>
2023-03-07 14:32:43 -0800

    Finished create new appointment form

--- ghi/app/src/CreateAppointment.js
+++ ghi/app/src/CreateAppointment.js
@@ -1,12 +1,99 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 const CreateAppointment = () => {
+	const [technicians, setTechnicians] = useState([]);
+
+	const [customerName, setCustomerName] = useState("");
+	const [vin, setVin] = useState("");
+	const [date, setDate] = useState("");
+	const [time, setTime] = useState("");
+	const [reason, setReason] = useState("");
+	const [technician, setTechnician] = useState("");
+
+	const handleFormChange = (event) => {
+		const value = event.target.value;
+		const formName = event.target.name;
+
+		if (formName === "customer_name") {
+			setCustomerName(value);
+		} else if (formName === "vin") {
+			setVin(value);
+		} else if (formName === "date") {
+			setDate(value);
+		} else if (formName === "time") {
+			setTime(value);
+		} else if (formName === "reason") {
+			setReason(value);
+		} else if (formName === "technician") {
+			setTechnician(value);
+		}
+	};
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+
+		const appointmentUrl = "http://localhost:8080/api/appointments/";
+
+		const isoDate = `${date}T00:00:00Z`;
+		console.log(isoDate);
+		const isoTime = `2000-10-20T${time}Z`;
+		console.log(isoTime);
+
+		const data = {
+			customer_name: customerName,
+			vin: vin,
+			date: isoDate,
+			time: isoTime,
+			reason: reason,
+			technician: technician,
+		};
+
+		const fetchConfig = {
+			method: "POST",
+			body: JSON.stringify(data),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		};
+
+		const response = await fetch(appointmentUrl, fetchConfig);
+
+		if (response.ok) {
+			const newAppointment = await response.json();
+			console.log(newAppointment);
+
+			setCustomerName("");
+			setVin("");
+			setDate("");
+			setTime("");
+			setReason("");
+			setTechnician("");
+		}
+	};
+
+	const mountTechnicians = async () => {
+		const technicianUrl = "http://localhost:8080/api/technicians/";
+		const response = await fetch(technicianUrl);
+
+		if (response.ok) {
+			const data = await response.json();
+			setTechnicians(data.technicians);
+		} else {
+			console.error(response);
+		}
+	};
+
+	useEffect(() => {
+		mountTechnicians();
+	}, []);
+
 	return (
 		<div className="row">
 			<div className="offset-3 col-6">
 				<div className="shadow p-4 mt-4">
 					<h1>Create a New Appointment</h1>
-					<form>
+					<form onSubmit={handleSubmit}>
 						<div className="mb-3">
 							<label htmlFor="customer_name" className="form-label">
 								Customer Name
@@ -17,7 +104,9 @@ const CreateAppointment = () => {
 								id="customer_name"
 								name="customer_name"
 								placeholder=""
-								required></input>
+								required
+								onChange={handleFormChange}
+								value={customerName}></input>
 						</div>
 						<div className="mb-3">
 							<label htmlFor="vin" className="form-label">
@@ -29,7 +118,9 @@ const CreateAppointment = () => {
 								id="vin"
 								name="vin"
 								placeholder=""
-								required></input>
+								required
+								onChange={handleFormChange}
+								value={vin}></input>
 						</div>
 						<div className="mb-3">
 							<label htmlFor="date" className="form-label">
@@ -41,7 +132,9 @@ const CreateAppointment = () => {
 								id="date"
 								name="date"
 								placeholder=""
-								required></input>
+								required
+								onChange={handleFormChange}
+								value={date}></input>
 						</div>
 						<div className="mb-3">
 							<label htmlFor="time" className="form-label">
@@ -53,7 +146,9 @@ const CreateAppointment = () => {
 								id="time"
 								name="time"
 								placeholder=""
-								required></input>
+								required
+								onChange={handleFormChange}
+								value={time}></input>
 						</div>
 						<div className="mb-3">
 							<label htmlFor="reason" className="form-label">
@@ -65,15 +160,26 @@ const CreateAppointment = () => {
 								id="reason"
 								name="reason"
 								placeholder=""
-								required></input>
+								required
+								onChange={handleFormChange}
+								value={reason}></input>
 						</div>
 						<div className="mb-3">
 							<select
 								id="technician"
 								name="technician"
 								className="form-select"
-								required>
+								required
+								onChange={handleFormChange}
+								value={technician}>
 								<option value="">Choose a technician</option>
+								{technicians.map((technician) => {
+									return (
+										<option key={technician.id} value={technician.id}>
+											{technician.name}
+										</option>
+									);
+								})}
 							</select>
 						</div>
 						<button type="submit" className="btn btn-primary">
